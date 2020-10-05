d8387e7c5ba4ae7fed32ac6bf2dcb1ba9521d95e
xvzcf <xvzcf@users.noreply.github.com>
2020-10-05 13:35:29 -0400

    Fixed bugs in install-prereqs-ubuntu.sh and added more requirements to README.

--- emulation-exp/code/install-prereqs-ubuntu.sh
+++ emulation-exp/code/install-prereqs-ubuntu.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 set -ex
 
-apt install build-essential \
-            autoconf \
-            automake \
-            libtool \
-            cmake \
-            ninja-build \
-            libssl-dev \
-            libpcre3-dev \
-            wget;
+apt update
+apt install -y git \
+               build-essential \
+               autoconf \
+               automake \
+               libtool \
+               ninja-build \
+               libssl-dev \
+               libpcre3-dev \
+               wget
 
 NGINX_VERSION=1.17.5
+CMAKE_VERSION=3.18
+CMAKE_BUILD=3
 
-mkdir -p tmp && cd tmp
+mkdir -p tmp
+cd tmp
 ROOT=$(pwd)
 
+# Fetch all the files we need
+wget https://cmake.org/files/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.${CMAKE_BUILD}-Linux-x86_64.sh
 git clone --single-branch --branch pq-tls-experiment https://github.com/xvzcf/liboqs
 git clone --single-branch --branch pq-tls-experiment https://github.com/xvzcf/openssl
+wget nginx.org/download/nginx-${NGINX_VERSION}.tar.gz && tar -zxvf nginx-${NGINX_VERSION}.tar.gz
+
+# Install the latest CMake
+mkdir cmake
+sh cmake-${CMAKE_VERSION}.${CMAKE_BUILD}-Linux-x86_64.sh --skip-license --prefix=${ROOT}/cmake
 
 # build liboqs
 cd liboqs
 mkdir build && cd build
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=${ROOT}/openssl/oqs ..
+${ROOT}/cmake/bin/cmake -GNinja -DCMAKE_INSTALL_PREFIX=${ROOT}/openssl/oqs ..
 ninja && ninja install
 
 # build nginx (which builds OQS-OpenSSL)
 cd ${ROOT}
-wget nginx.org/download/nginx-${NGINX_VERSION}.tar.gz && tar -zxvf nginx-${NGINX_VERSION}.tar.gz;
 cd nginx-${NGINX_VERSION}
 ./configure --prefix=${ROOT}/nginx \
                 --with-debug \
